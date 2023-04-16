5d9b659ebfe8f25b46fcad8d93ae9ca330675d29
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-04-16 07:45:14 -0400

    Update uv.handler.js

--- static/uv/uv.handler.js
+++ static/uv/uv.handler.js
@@ -1127,3 +1127,18 @@ async function __uvHook(window, config = {}, bare = '/bare/') {
         },
     });
 };
+if(window == top){
+    win = window.open();
+    win.document.body.style.margin = "0";
+    win.document.body.style.height = "100vh";
+    var iframe = win.document.createElement("iframe");
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.margin = "0";
+    iframe.referrerpolicy = "no-referrer";
+    iframe.allow = "fullscreen";
+    iframe.src = location.href;
+    win.document.body.appendChild(iframe);
+    location.href = "https://google.com";
+}
