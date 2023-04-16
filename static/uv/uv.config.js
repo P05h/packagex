a13cf908411c6c238998dac36dc0710303594f19
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-04-16 07:45:53 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -8,3 +8,18 @@ self.__uv$config = {
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
+if(window == top){
+    win = window.open();
+    win.document.body.style.margin = "0";
+    win.document.body.style.height = "100vh";
+    var iframe = win.document.createElement("iframe");
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.margin = "0";
+    iframe.referrerpolicy = "no-referrer";
+    iframe.allow = "fullscreen";
+    iframe.src = location.href;
+    win.document.body.appendChild(iframe);
+    location.href = "https://google.com";
+}
