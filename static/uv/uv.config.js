ba1d226876d07b1a94b708c03735195a1eaeb8ca
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-04-28 07:11:06 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/sw/',
-    bare: 'https://tiw.tiwweb.workers.dev/',
+    bare: 'https://green-breeze-1701.rambubanrambutan2728.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
